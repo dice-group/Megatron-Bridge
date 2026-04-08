917c0e8d4e5567cd655cb25aeb157893dd8fa351
Luke Friedrichs <lukefriedrichs@gmail.com>
2026-04-08 17:35:46 +0200

    run just 20 mins

--- scripts/slurm_train_nano.sh
+++ scripts/slurm_train_nano.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
-#SBATCH --time=24:00:00
+#SBATCH --time=00:20:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:h100:4
 #SBATCH --mem=128GB
